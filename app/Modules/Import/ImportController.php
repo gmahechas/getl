e58d3bd5016ed4cf9efc0eeae46c846f3fba68ff
gmahechas <gmahechas@ingenieros.com>
2020-08-13 21:56:28 -0500

    finish uploads

--- app/Modules/Import/ImportController.php
+++ app/Modules/Import/ImportController.php
@@ -15,6 +15,8 @@ class ImportController extends Controller
     public function store(Request $request)
     {
         /* $import_type = $request->import_type; */
+        $file_name = $request->file('file')->getClientOriginalName();
+        echo $file_name;
         $path = $request->file('file')->store('uploads');
 
         return $path;
